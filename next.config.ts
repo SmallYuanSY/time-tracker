d7544378192ad20c17a825199c0e5688de6ff35e
まぅSmallYuan <50947770+SmallYuanSY@users.noreply.github.com>
2025-07-02 17:25:21 +0800

    更新 Next.js 配置中的內容安全政策，將 X-Frame-Options 的值從 'DENY' 調整為 'SAMEORIGIN'，並在 script-src 中新增 frame-src 以支持 https://vercel.live，提升應用的安全性與靈活性。

--- next.config.ts
+++ next.config.ts
@@ -23,11 +23,11 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; connect-src 'self' https://*.prisma.io https://*.novu.co https://*.vercel.app https://vercel.live wss://*.novu.co; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.vercel.live https://vercel.live; script-src-elem 'self' 'unsafe-inline' https://*.vercel.live https://vercel.live; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:;"
+            value: "default-src 'self'; connect-src 'self' https://*.prisma.io https://*.novu.co https://*.vercel.app https://vercel.live wss://*.novu.co; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.vercel.live https://vercel.live; script-src-elem 'self' 'unsafe-inline' https://*.vercel.live https://vercel.live; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; frame-src 'self' https://*.vercel.live https://vercel.live;"
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY'
+            value: 'SAMEORIGIN'
           },
           {
             key: 'X-Content-Type-Options',
